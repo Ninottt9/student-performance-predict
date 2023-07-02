1d1042bc9ae73a9b5a5193cb8e3d86c6a9acea25
Pazuzik <zuzia.sulima@gmail.com>
2023-07-02 13:05:48 +0200

    Added more rules to fuzzy logic

--- fuzzy.py
+++ fuzzy.py
@@ -22,7 +22,12 @@ class FuzzyControls:
     def init_control_system(self):
         self.rule1 = ctrl.Rule(self.study_time['low'] & self.absences['low'] & self.health['low'] & self.alcohol['low'], self.grade['low'])
         self.rule2 = ctrl.Rule(self.study_time['medium'] & self.absences['medium'] & self.health['medium'] & self.alcohol['medium'], self.grade['medium'])
-        self.rule3 = ctrl.Rule(self.study_time['high'] & self.absences['high'] & self.health['high'] & self.alcohol['high'], self.grade['high'])
+        self.rule3 = ctrl.Rule(self.study_time['high'] & self.absences['low'] & self.health['high'] & self.alcohol['low'], self.grade['high'])
+        self.rule4 = ctrl.Rule(self.study_time['medium'] & self.absences['low'] & self.health['high'] & self.alcohol['low'], self.grade['medium'])
+        self.rule5 = ctrl.Rule(self.study_time['high'] & self.absences['low'] & self.health['high'] & self.alcohol['low'], self.grade['high'])
+        self.rule6 = ctrl.Rule(self.study_time['low'] & self.absences['low'] & self.health['high'] & self.alcohol['low'], self.grade['medium'])
+        self.rule7 = ctrl.Rule(self.study_time['medium'] & self.absences['medium'] & self.health['medium'] & self.alcohol['high'], self.grade['low'])
+        self.rule8 = ctrl.Rule(self.study_time['low'] & self.absences['medium'] & self.health['high'] & self.alcohol['high'], self.grade['low'])
 
-        self.control_system = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3])
+        self.control_system = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3, self.rule4, self.rule5, self.rule6, self.rule7, self.rule8])
         self.control_simulation = ctrl.ControlSystemSimulation(self.control_system)
